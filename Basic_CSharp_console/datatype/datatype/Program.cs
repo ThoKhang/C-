34a4b4c28193da72e88f2f891b0c5ecc78fb91f3
ThoKhang <khangheheqt@gmail.com>
2025-08-27 22:14:20 +0700

    datatype in Csharp

---
+++ Basic_CSharp_console/datatype/datatype/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double pi;
+pi = 3.14;
+double two_pi;
+two_pi = 2 * pi;
+/*
+    Console.Title = "Vi du su dung console";
+    Console.ForegroundColor = ConsoleColor.Green;
+    string fullName;
+    Console.WriteLine("please let me get your full name: ");
+    fullName = Console.ReadLine();
+    Console.WriteLine("Hi, please input and output to console");
+    Console.WriteLine(two_pi);
+    Console.WriteLine("full name: {0}", fullName);
+*/
+
+float a, b;
+string input;
+Console.WriteLine("please enter a: ");
+a = float.Parse(Console.ReadLine());
+Console.WriteLine("please enter b:");
+b = float.Parse(Console.ReadLine());
+Console.WriteLine("a = {0}, b = {1}",a,b);
+//default data type in Csharp
+var am = "hello";
+//const in Cscharp
+const double aa = 10.2;
