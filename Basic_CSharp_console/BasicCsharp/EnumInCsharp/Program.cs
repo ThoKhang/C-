12d8cae6f450fa0c2407f1d89228c6f94fb6512e
ThoKhang <khangheheqt@gmail.com>
2025-08-28 09:14:31 +0700

    enum and struct

---
+++ Basic_CSharp_console/BasicCsharp/EnumInCsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumInCsharp
+{
+    internal class Program
+    {
+        enum WeekDay
+        {
+            Monday,     // = 0
+            Tuesday,    // = 1
+            Wednesday,  // = 2
+            Thursday,   // = 3
+            Friday,     // = 4
+            Saturday,   // = 5
+            Sunday      // = 6
+        }
+        static void Main(string[] args)
+        {
+            WeekDay today = WeekDay.Monday;
+            Console.WriteLine(today);
+            Console.WriteLine((int)today);
+            
+
+        }
+    }
+}
---
+++ Basic_CSharp_console/BasicCsharp/struct/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @struct
+{
+    internal class Program
+    {
+        struct threeValues
+        {
+            public int a, b, c;
+            public threeValues (int a, int b, int c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+            public int total(int a, int b, int c)
+            {
+                return (a + b + c);
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            
+            int a, b, c;
+            Console.WriteLine("enter a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter c: ");
+            c = int.Parse(Console.ReadLine());
+            threeValues firstStep = new threeValues (a, b, c);
+            Console.WriteLine("perimeter values: {0}", firstStep.total(a, b, c));
+        }
+    }
+}
