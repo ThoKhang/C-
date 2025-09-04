97756ae28a228247e8cc4ce0e09eb355c7d78967
ThoKhang <khangheheqt@gmail.com>
2025-09-04 09:35:14 +0700

    new oop

--- Basic_CSharp_console/BasicCsharp/OOPC#/Program.cs
+++ Basic_CSharp_console/BasicCsharp/OOPC#/Program.cs
@@ -11,13 +11,19 @@ namespace OOPC_
         static void Main(string[] args)
         {
             string name;
-            double quantity;
-            int price, damage;
+            int quantity, damage;
+            double price;
             Console.WriteLine("please enter the real name of shotgun: ");
             name = Console.ReadLine();
-            Console.WriteLine("quantity: ");
-            quantity = double.Parse(Console.ReadLine());
             Console.WriteLine("how many: ");
+            quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine("how much: ");
+            price = int.Parse(Console.ReadLine());
+            Console.WriteLine("damage in game: ");
+            damage = int.Parse(Console.ReadLine());
+            weapon shotGun = new weapon(name, quantity, damage, price);
+            Console.WriteLine("success !");
+            Console.WriteLine("Data: {0}, {1}, {2}, {3}", shotGun.name, shotGun.quantity, shotGun.damage, shotGun.price);
 
         }
     }
