4d6ba0cad1bcc3132757eb1ecf4649405b5fd97f
ThoKhang <khangheheqt@gmail.com>
2025-09-10 23:34:43 +0700

    fix

---
+++ Basic_CSharp_console/BasicCsharp/basicOOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int damage;
+            double price;
+            VuKhi vuKhi;
+            Console.WriteLine("please enter name of weapon: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter damage: ");
+            damage = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the price of weapon: ");
+            price = double.Parse(Console.ReadLine());
+            vuKhi = new VuKhi(name, damage, price);
+            Console.WriteLine("This is your weapon: {0}, {1}, {2}", vuKhi.getName(), vuKhi.getDamage(), vuKhi.getPrice());
+        }
+    }
+}
---
+++ Basic_CSharp_console/BasicCsharp/basicOOP/VuKhi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicOOP
+{
+    internal class VuKhi
+    {
+        string name;
+        int damage;
+        double price;
+        public VuKhi(string name, int damage, double price)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.price = price;
+        }
+        public VuKhi() { }
+        public int damageTaken (int damage)
+        {
+            return damage;   
+        }
+        public void priceTaken(double price)
+        {
+            this.price = price;
+        }
+        public int getDamage()
+        {
+            return damage;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+    }
+}
