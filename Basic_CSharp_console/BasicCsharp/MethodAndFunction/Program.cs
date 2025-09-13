b83b447cc0fd4b73becea54e1830193c3b86681f
ThoKhang <khangheheqt@gmail.com>
2025-09-13 20:37:11 +0700

    function anh method

---
+++ Basic_CSharp_console/BasicCsharp/MethodAndFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndFunction
+{
+    internal class Program
+    {
+        //static method, static function
+        public static void helloWorld()
+        {
+            System.Console.WriteLine("hello Csharp");
+        }
+        public static float adition (float a, float b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {   
+            helloWorld();
+            int a = 3, b = 4;
+            Console.WriteLine("{0}", adition(a, b));
+        }
+    }
+}
