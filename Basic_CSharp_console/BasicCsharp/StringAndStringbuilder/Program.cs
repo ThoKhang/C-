7c6bfa83946a2d8d17b340f79cf4cdca8c713a3b
ThoKhang <khangheheqt@gmail.com>
2025-09-13 20:48:42 +0700

    string and stringbuilder

---
+++ Basic_CSharp_console/BasicCsharp/StringAndStringbuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndStringbuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //phép nối chuỗi
+            Console.WriteLine("Please enter a greeting: ");
+            string greetings = Console.ReadLine();
+            Console.WriteLine("Please enter your full name: ");
+            string fullName = Console.ReadLine();
+            string newString = greetings + " " + fullName;
+            Console.WriteLine(newString);  
+        }
+    }
+}
