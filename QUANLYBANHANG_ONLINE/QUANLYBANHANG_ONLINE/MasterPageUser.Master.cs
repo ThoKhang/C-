ed4ff3a69bf69009b17340be2aaee481928c120b
ThoKhang <khangheheqt@gmail.com>
2025-09-23 12:38:26 +0700

    demo

---
+++ QUANLYBANHANG_ONLINE/QUANLYBANHANG_ONLINE/MasterPageUser.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QUANLYBANHANG_ONLINE
+{
+    public partial class MasterPageUser : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
