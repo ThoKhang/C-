b938820b54415323f4c692f067b74fa27b051fba
ThoKhang <khangheheqt@gmail.com>
2025-09-23 16:37:42 +0700

    chitietsanpham

---
+++ QUANLYBANHANG_ONLINE/QUANLYBANHANG_ONLINE/CHITIETSANPHAM.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QUANLYBANHANG_ONLINE
+{
+    public partial class CHITIETSANPHAM : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XULYDULIEU xuly = new XULYDULIEU();
+            String masanpham = Request.QueryString.Get("MASANPHAM");
+            SqlParameter[] pr = new SqlParameter[1];
+            pr[0] = new SqlParameter("MASANPHAM", masanpham);
+            this.Repeater1.DataSource = xuly.getTable("psGetTableSANPHAM",pr);
+            this.Repeater1.DataBind();
+        }
+    }
+}
---
+++ QUANLYBANHANG_ONLINE/QUANLYBANHANG_ONLINE/DANHSACHSANPHAM.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+namespace QUANLYBANHANG_ONLINE
+{
+    public partial class DANHSACHSANPHAM : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XULYDULIEU xyly = new XULYDULIEU();
+            SqlParameter[] pr = new SqlParameter[1];
+            pr[0] = new SqlParameter("@MASANPHAM",DBNull.Value);
+            this.DataList1.RepeatColumns = 3;
+            this.DataList1.DataSource = xyly.getTable("psGetTableSANPHAM", pr);
+            this.DataList1.DataBind();
+        }
+        
+    }
+}
