cfc730799c1d925c75dc1afdd10ca508bb80da47
ThoKhang <khangheheqt@gmail.com>
2025-10-05 22:54:53 +0700

    OOP

---
+++ Basic_CSharp_console/BasicCsharp/KeThua/cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeThua
+{
+    internal class cat : animal
+    {
+        public string Food;
+        public cat() 
+        {
+            this.Legs = 4;
+            this.Food = "Mouse";
+            this.ShowLeg();
+        }
+        public void Eat()
+        {
+            Console.WriteLine(Food);
+        }
+    }
+}
