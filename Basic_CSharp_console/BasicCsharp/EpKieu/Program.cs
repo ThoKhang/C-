f32c4310232878331fa38f91cf01f949a85adccd
ThoKhang <khangheheqt@gmail.com>
2025-10-05 23:18:44 +0700

    namespace and partial

---
+++ Basic_CSharp_console/BasicCsharp/EpKieu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpKieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A thuộc cơ sở 
+            //B con A
+            //C con B
+            //A có thể gán B,C
+            //B có thể gán C
+            //C không thể gán cho ai
+        }
+    }
+}
---
+++ Basic_CSharp_console/BasicCsharp/NamespaceAndPartical/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceAndPartical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            class1.XinChao();
+            Abc.class2.XinChao();
+        }
+    }
+}
