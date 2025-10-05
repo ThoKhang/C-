cda0127db224b9928b04cc96d251059cff23aeef
ThoKhang <khangheheqt@gmail.com>
2025-10-05 23:49:18 +0700

    partial and class in class

--- Basic_CSharp_console/BasicCsharp/NamespaceAndPartical/Program.cs
+++ Basic_CSharp_console/BasicCsharp/NamespaceAndPartical/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
+using static System.Math;
 namespace NamespaceAndPartical
 {
     internal class Program
@@ -12,6 +13,8 @@ namespace NamespaceAndPartical
         {
             class1.XinChao();
             Abc.class2.XinChao();
+            //Phương thức tĩnh 
+            WriteLine(PI);
         }
     }
 }
---
+++ Basic_CSharp_console/BasicCsharp/partial/Product2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial
+{
+    internal partial class Product1
+    {
+       public string description { get; set; }
+    }
+}
